35622e801a5500badf04f65ea6968b3ad2221825
1oannis <thio1011@h-ka.de>
2024-09-12 19:58:56 +0900

    feat lab-1 complete

---
+++ lab-1/README.md
@@ -0,0 +1,35 @@
+# Python Lab #1: Korean COVID-19 New Cases by Region
+> [!NOTE]
+> Copyright 2024 - present [Ioannis Theodosiadis](mailto:ioannis@seoultech.ac.kr), SEOULTECH University
+>
+> This program is free software: you can redistribute it and/or modify
+> it under the terms of the GNU General Public License as published by
+> the Free Software Foundation, either version 3 of the License, or
+> at your option any later version
+>
+> This program is distributed in the hope that it will be useful
+> but WITHOUT ANY WARRANTY; without even the implied warranty of
+> MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+> GNU General Public License for more details
+>
+> You should have received a copy of the GNU General Public License
+> along with this program. If not, see <https://www.gnu.org/licenses/>
+
+## Assignment
+- Complete the given skeleton code (covid19_statistics_skeleton.py)
+- Submit your code (covid19_statistics.py) and its output copy (covid19_statistics.md)
+  - You need to copy and paste your printed output to a text file (covid19_statistics.md).
+
+## How To Run It
+Change into the lab-1 directory.
+```PowerShell
+cd lab-1
+```
+
+Run the Python script.
+```PowerShell
+python .\covid19_statistics_skeleton.py
+```
+
+> [!IMPORTANT]
+> The results are directly written into the [covid19_statistics.md](./covid19_statistics.md) file.
---
+++ lab-1/covid19_statistics_skeleton.py
@@ -0,0 +1,97 @@
+def normalize_data(n_cases, n_people, scale):
+    norm_cases = []
+    for i in range(len(n_cases)):
+        norm_cases.append(n_cases[i] / n_people[i] * scale)
+    return norm_cases
+
+
+regions = [
+    "Seoul",
+    "Gyeongi",
+    "Busan",
+    "Gyeongnam",
+    "Incheon",
+    "Gyeongbuk",
+    "Daegu",
+    "Chungnam",
+    "Jeonnam",
+    "Jeonbuk",
+    "Chungbuk",
+    "Gangwon",
+    "Daejeon",
+    "Gwangju",
+    "Ulsan",
+    "Jeju",
+    "Sejong",
+]
+n_people = [
+    9550227,
+    13530519,
+    3359527,
+    3322373,
+    2938429,
+    2630254,
+    2393626,
+    2118183,
+    1838353,
+    1792476,
+    1597179,
+    1536270,
+    1454679,
+    1441970,
+    1124459,
+    675883,
+    365309,
+]  # 2021-08
+n_covid = [
+    644,
+    529,
+    38,
+    29,
+    148,
+    28,
+    41,
+    62,
+    23,
+    27,
+    27,
+    33,
+    16,
+    40,
+    20,
+    5,
+    4,
+]  # 2021-09-21
+
+sum_people = sum(n_people)  # The total number of people
+sum_covid = sum(n_covid)  # The total number of new cases
+norm_covid = normalize_data(
+    n_covid, n_people, 1000000
+)  # The new cases per 1 million people
+
+try:
+    with open('covid19_statistics.md', 'w', encoding='utf-8') as f:
+        # Print population by region
+        f.write("### Korean Population by Region\n")
+        f.write(f"* Total population: {sum_people}\n\n")
+        f.write("| Region | Population | Ratio (%) |\n")
+        f.write("| ------ | ---------- | --------- |\n")
+        for idx, pop in enumerate(n_people):
+            ratio = pop / sum_people * 100
+            f.write(f"| {regions[idx]} | {pop} | {ratio:.1f} |\n")
+        f.write("\n")
+
+        # Print COVID-19 new cases by region
+        f.write("### Korean COVID-19 New Cases by Region\n")
+        f.write(f"* Total new cases: {sum_covid}\n\n")
+        f.write("| Region | New Cases | Ratio (%) | New cases / 1M |\n")
+        f.write("| ------ | ---------- | --------- | -------------- |\n")
+        for idx, cases in enumerate(n_covid):
+            ratio = cases / sum_covid * 100
+            f.write(
+                f"| {regions[idx]} | {cases} | {ratio:.1f} | {norm_covid[idx]:.1f} |\n")
+        f.write("\n")
+except IOError as e:
+    print(f"An error occurred while writing to the file: {e}")
+else:
+    print("The results are saved in covid19_statistics.md")
